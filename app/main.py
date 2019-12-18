d68d5ca3988506508cfd813968bcfac407466c90
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2019-12-18 23:09:48 +0200

    Add CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
+from flask_cors import CORS
 
 from app.api import blueprint as api
 
@@ -12,6 +14,8 @@ web_app.config.from_object('config')
 web_app.register_blueprint(api, url_prefix='/api/v1')
 
 db = SQLAlchemy(web_app)
+heroku = Heroku(web_app)
+cors = CORS(web_app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     web_app.run("0.0.0.0", 8080)
